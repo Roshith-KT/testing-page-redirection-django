9b7294d290704c34b99b7549362e078d8a8b918b
Roshith K T <123396675+Roshith-KT@users.noreply.github.com>
2023-08-09 14:28:41 +0530

    urls and view function added for target page

--- redirectapp/urls.py
+++ redirectapp/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.redirect_page, name='redirect_page'),
+    path('target/', views.target_page, name='target_page'),
     # Other URL patterns
 ]
--- redirectapp/views.py
+++ redirectapp/views.py
@@ -6,3 +6,5 @@ from django.shortcuts import render, redirect
 def redirect_page(request):
     return render(request, 'redirect_page.html')
 
+def target_page(request):
+    return render(request, 'target_page.html')  # Replace with your actual target page template
